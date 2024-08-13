afcb92bd765a4b036cf3498bb58b95edbb4fbb23
Yarda89 <56601456+Yarda89@users.noreply.github.com>
2024-08-14 01:26:13 +0200

    sensor fault - no crash

--- PicoInk_code/main.py
+++ PicoInk_code/main.py
@@ -1,3 +1,30 @@
+from measurement import batt_voltage, voltage_to_soc
+
+battery_voltage = batt_voltage()
+if battery_voltage < 2.6:                                   # turn off immediatelly to prevent draining battery
+    from machine import Pin
+    Pin(9, Pin.OUT).value(1)
+
+
+if battery_voltage < 2.7:                                 # show battery dead screen then turn off
+    from gpio_definitions import DONE_PIN, RED_LED
+    from lib.display import screens
+    from lib.templates import Low_batt_image
+    from time import sleep_ms
+
+    screens.widgets.clear()
+    screens.widgets.image(Low_batt_image)
+    screens.eink.show(screens.widgets.img, partial=False)
+    sleep_ms(100)
+    RED_LED.value(1)
+    screens.eink.deep_sleep()
+    sleep_ms(100)
+    RED_LED.value(0)
+    DONE_PIN.value(1)
+    sleep_ms(1_000)
+
+
+# Start Wireless ASAP
 from nonvolatile import Settings, settings_save, settings_load
 settings_load()
 wifi_active = Settings["WiFi-SSID"]
@@ -8,21 +35,18 @@ ble_active = Settings["BLE-name"]
 if ble_active:
     from lib.wireless.ble_advert import ble_advert, ble
 
-from time import sleep_ms
-from gpio_definitions import BTN_1, BTN_2, BTN_3, GREEN_LED, DONE_PIN
-from measurement import batt_voltage, voltage_to_soc
+
+from gpio_definitions import BTN_1, BTN_2, BTN_3, GREEN_LED, DONE_PIN, RED_LED
 from lib.display import screens
+from time import sleep_ms
 from sensor import sensor
 
-
 if __name__ == '__main__':
-    battery_voltage = batt_voltage()
-    if battery_voltage < 2.8:
-        DONE_PIN.value(1)
-    bat_soc = voltage_to_soc(battery_voltage)
-
     device_run = True
     full_refresh = False
+
+    bat_soc = voltage_to_soc(battery_voltage)
+
     if not BTN_1.value():
         from modes.mode_setup import start_setup
         start_setup("PICOINK", battery_voltage)
@@ -53,28 +77,34 @@ if __name__ == '__main__':
         full_refresh = True
 
     if device_run:
-        # mqtt_payload = {}
         from modes.mode_regular import load_show_save
         while True:
-            load_show_save(full_refresh, bat_soc, sensor)
+            everything_ok = True
+            sensor_ok = load_show_save(full_refresh, bat_soc, sensor)
+            if not sensor_ok:
+                everything_ok = False
             sensor.last_values["soc"] = bat_soc
             if wifi_active:
-                rssi = wait_for_wifi_connection()
+                if sensor_ok:
+                    rssi = wait_for_wifi_connection()
+                else:
+                    STA.disconnect()
+                    rssi = None
                 if rssi:
                     from lib.wireless.ha import send_state, connect_mqtt, MQTT
                     if connect_mqtt():
-                        GREEN_LED.value(1)
                         sensor.last_values["signal"] = rssi
                         send_state(**sensor.last_values)
                         MQTT.wait_msg()
                         MQTT.disconnect()
                         sleep_ms(500)
-                    # screens.widgets.rect()
                     screens.widgets.signal_indicator(rssi)
                     screens.eink.show(screens.widgets.img, partial=True)
+                else:
+                    everything_ok = False
                 STA.disconnect()
 
-            if ble_active:
+            if ble_active and sensor_ok:
                 ble_advert(sensor.get_ble_characteristics())
                 sleep_ms(500)
                 ble.gap_advertise(None)
@@ -82,9 +112,15 @@ if __name__ == '__main__':
             if wifi_active:
                 sleep_ms(1000)
 
-            GREEN_LED.value(0)
+            if everything_ok:
+                GREEN_LED.value(1)
+            else:
+                RED_LED.value(1)
+
             screens.eink.deep_sleep()
             sleep_ms(100)
+            GREEN_LED.value(0)
+            RED_LED.value(0)
             DONE_PIN.value(1)
             sleep_ms(10_000)
 
--- PicoInk_code/modes/mode_regular.py
+++ PicoInk_code/modes/mode_regular.py
@@ -5,7 +5,13 @@ from nonvolatile import Settings, save_value, get_last_values, num_to_byte, byte
 def load_show_save(full_refresh, bat_soc, sensor):
     minimum = sensor.displ_min
     maximum = sensor.displ_max
-    sensor_ok = sensor.get_values()
+    try:
+        sensor_ok = sensor.get_values()
+    except Exception as e:
+        print(e)
+        sensor_ok = False
+        return sensor_ok
+
     value = list(sensor.last_values.items())[0][1]
 
     filesize, byte_values = get_last_values(249, "temperatures.dat")
